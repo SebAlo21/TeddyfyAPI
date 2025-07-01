bb4ee3c909f406b5184608042bd8d38af1120832
alonso <alonsosrv@gmail.com>
2025-07-01 13:02:52 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const cors = require('cors');
+const productos = require('./productos.json');
+
+
+// Server Configuration
+const app = express()
+
+//Cors
+app.use(cors())
+
+//Contenido
+app.get('/api/productos',(req,res) => {
+    res.json(productos)
+});
+
+//PORT
+const PORT  = process.env.PORT || 3000;
+
+app.listen(PORT,() => {
+    console.log("Api ok")
+})
